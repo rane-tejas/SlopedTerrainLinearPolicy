723a0882b3280fdb85b584b28bd3222703b62b20
Tejas Rane <f20170918@goa.bits-pilani.ac.in>
2021-04-18 13:07:27 +0530

    Commit before adding foot step planner

--- gym_sloped_terrain/envs/trajectory_generator.py
+++ gym_sloped_terrain/envs/trajectory_generator.py
@@ -28,6 +28,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 PI = np.pi
+no_of_points = 100
 
 @dataclass
 class leg_data:
@@ -60,11 +61,12 @@ class robot_data:
 
 
 class TrajectoryGenerator():
-    def __init__(self, gait_type='trot', phase=[0, PI, PI, 0]):
+    def __init__(self, gait_type='trot', phase=[0, 100, 100, 0]):
         self.gait_type = gait_type
-        self._phase = robot_data(front_left=phase[0], front_right=phase[1], back_left=phase[2], back_right=phase[3])
+        self.phase = robot_data(front_left=phase[0], front_right=phase[1], back_left=phase[2], back_right=phase[3])
         self.frequency = 2.5
-        self.omega = 2 * PI * self.frequency
+        # self.omega = 2 * PI * self.frequency
+        self.omega = 2 * no_of_points * self.frequency
         self.theta = 0
 
         self.front_left = leg_data('FL', 1)
@@ -79,24 +81,37 @@ class TrajectoryGenerator():
 
         self.foot_clearance = 0.06
         self.walking_height = -0.25
+        self.swing_time = 0.25
 
     def update_leg_theta(self, dt):
         '''
         Function to calculate the leg cycles of the trajectory, depending on the gait.
         '''
 
+        # def constrain_theta(theta):
+        #     theta = np.fmod(theta, 2 * PI)
+        #     if (theta < 0):
+        #         theta = theta + 2 * PI
+        #     return theta
+
         def constrain_theta(theta):
-            theta = np.fmod(theta, 2 * PI)
+            theta = np.fmod(theta, 2 * no_of_points)
             if (theta < 0):
-                theta = theta + 2 * PI
+                theta = theta + 2 * no_of_points
             return theta
 
         self.theta = constrain_theta(self.theta + self.omega * dt) 
 
-        self.front_left.theta = constrain_theta(self.theta + self._phase.front_left)
-        self.front_right.theta = constrain_theta(self.theta + self._phase.front_right)
-        self.back_left.theta = constrain_theta(self.theta + self._phase.back_left)
-        self.back_right.theta = constrain_theta(self.theta + self._phase.back_right)
+        # self.front_left.theta = constrain_theta(self.theta + self.phase.front_left) 
+        # self.front_right.theta = constrain_theta(self.theta + self.phase.front_right)
+        # self.back_left.theta = constrain_theta(self.theta + self.phase.back_left)
+        # self.back_right.theta = constrain_theta(self.theta + self.phase.back_right)
+
+        #converting no of points notation to PI
+        self.front_left.theta = constrain_theta(self.theta + self.phase.front_left) * (2*PI)/(2*no_of_points) 
+        self.front_right.theta = constrain_theta(self.theta + self.phase.front_right) * (2*PI)/(2*no_of_points)
+        self.back_left.theta = constrain_theta(self.theta + self.phase.back_left) * (2*PI)/(2*no_of_points)
+        self.back_right.theta = constrain_theta(self.theta + self.phase.back_right) * (2*PI)/(2*no_of_points)
 
     def reset_theta(self):
         '''
@@ -146,6 +161,8 @@ class TrajectoryGenerator():
         self.back_right.prev_motor_knee = prev_motor_angles[7]
         self.back_right.prev_motor_abd = prev_motor_angles[11]
 
+    def foot_step_planner(self):
+
     def calculate_planar_traj(self, leg, v_x, v_y, w_z, dt):
         '''
         Calculates the x and y component of the trajectory based on the commanded velocities (either from joystick or augmented by the policy).
@@ -179,7 +196,7 @@ class TrajectoryGenerator():
         v_acomp = np.cross(cmd_avel, prev_r)
         v_leg = v_lcomp + v_acomp
 
-        if leg.theta > PI: # theta taken from +x, CW # Flip this sigh if the trajectory is mirrored
+        if leg.theta > PI: # theta taken from +x, CW # Flip this sign if the trajectory is mirrored
             flag = -1 #during stance_phase of walking, leg moves backwards to push body forward
         else:
             flag = 1 #during swing_phase of walking, leg moves forward to next step
@@ -187,8 +204,8 @@ class TrajectoryGenerator():
         dr = v_leg * dt * flag
         r = prev_r + dr - np.array(leg_frame)
 
-        x = r[0] + leg.x_shift
-        y = r[1] + leg.y_shift
+        x = r[0] #+ leg.x_shift
+        y = r[1] #+ leg.y_shift
 
         return [x, y]
 
@@ -251,6 +268,7 @@ class TrajectoryGenerator():
         for leg in legs:
 
             [leg.prev_x, leg.prev_y, leg.prev_z] = self.stochlite_kin.forwardKinematics(leg.name, [leg.prev_motor_abd, leg.prev_motor_hip, leg.prev_motor_knee])
+            # [leg.prev_x, leg.prev_y, leg.prev_z] = [leg.x, leg.y, leg.z]
             [leg.x, leg.y] = self.calculate_planar_traj(leg, lin_vel_x, lin_vel_y, ang_vel_z, dt)
             leg.z = self.calculate_vert_comp(leg)
 
@@ -266,10 +284,17 @@ class TrajectoryGenerator():
                             legs.front_left.motor_abd, legs.front_right.motor_abd,
                             legs.back_left.motor_abd, legs.back_right.motor_abd]
         
-        fl_foot_pos = [legs.front_left.x + self.robot_length/2, legs.front_left.y + self.robot_width/2, legs.front_left.z]
-        fr_foot_pos = [legs.front_right.x + self.robot_length/2, legs.front_right.y - self.robot_width/2, legs.front_right.z]
-        bl_foot_pos = [legs.back_left.x - self.robot_length/2, legs.back_left.y + self.robot_width/2, legs.back_left.z]
-        br_foot_pos = [legs.back_right.x - self.robot_length/2, legs.back_right.y - self.robot_width/2, legs.back_right.z]
+        # Plotting in base frame
+        # fl_foot_pos = [legs.front_left.x + self.robot_length/2, legs.front_left.y + self.robot_width/2, legs.front_left.z]
+        # fr_foot_pos = [legs.front_right.x + self.robot_length/2, legs.front_right.y - self.robot_width/2, legs.front_right.z]
+        # bl_foot_pos = [legs.back_left.x - self.robot_length/2, legs.back_left.y + self.robot_width/2, legs.back_left.z]
+        # br_foot_pos = [legs.back_right.x - self.robot_length/2, legs.back_right.y - self.robot_width/2, legs.back_right.z]
+
+        # Plotting in leg frame
+        fl_foot_pos = [legs.front_left.x, legs.front_left.y, legs.front_left.z]
+        fr_foot_pos = [legs.front_right.x, legs.front_right.y, legs.front_right.z]
+        bl_foot_pos = [legs.back_left.x, legs.back_left.y, legs.back_left.z]
+        br_foot_pos = [legs.back_right.x, legs.back_right.y, legs.back_right.z]
         
         return leg_motor_angles, fl_foot_pos + fr_foot_pos + bl_foot_pos + br_foot_pos
 
@@ -278,7 +303,7 @@ if __name__ == '__main__':
     action = np.array([0.0, 0.0, 0.0, 0.0,
                        0.0, 0.0, 0.0, 0.0,
                        0.0, 0.0, 0.0, 0.0,
-                       0.5, 0.0, 0.0])
+                       0.1, 0.0, 0.0])
     
     plotdata = []
     dt = 0.01
@@ -304,7 +329,7 @@ if __name__ == '__main__':
 
     ax.plot3D(x_fl, y_fl, z_fl, 'red')
     ax.plot3D(x_fr, y_fr, z_fr, 'blue')
-    ax.plot3D(x_bl, y_bl, z_bl, 'blue')
-    ax.plot3D(x_br, y_br, z_br, 'red')
+    # ax.plot3D(x_bl, y_bl, z_bl, 'blue')
+    # ax.plot3D(x_br, y_br, z_br, 'red')
 
     plt.show()
